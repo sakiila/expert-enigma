89fb03a1ac43a159b900d0fcb3289add6a3cbd7d
Bob Bao <me@baobo.me>
2024-01-10 21:21:56 +0800

    init

---
+++ src/main/java/me/baobo/engima/EngimaApplication.java
@@ -0,0 +1,13 @@
+package me.baobo.engima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/baobo/engima/EngimaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.baobo.engima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
